699e7b66f869a0e57477a076f952712416e83880
Boss El <edatsomor010@st.ug.edu.gh>
2024-08-28 02:13:14 +0700

    chore: Update signup endpoint to use error handling middleware

--- api/index.js
+++ api/index.js
@@ -21,3 +21,12 @@ mongoose.connect(process.env.MONGO)
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+
+
+//this is the error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({ success: false, statusCode, message });
+}
+);
