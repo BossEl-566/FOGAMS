acea7d6ec0b4a7fc5ff0b5e2d26b8de2ab05c7bb
Boss El <edatsomor010@st.ug.edu.gh>
2024-08-27 09:50:08 +0700

    created header section

---
+++ Client-FOGA/src/components/Header.jsx
@@ -0,0 +1,71 @@
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+
+export default function Header() {
+  const path = useLocation().pathname;
+  
+
+  return (
+    <Navbar className='border-b-2'>
+      <Link to='/' className='self-center whitespace-nowrap'>
+        <div className='flex items-center'>
+          <img src='/src/assets/assembliesOfGodLogo.png' alt='Assemblies of God Logo' width='30' height='20' />
+          <div className='w-px h-8 bg-black ml-1 dark:bg-white'></div>
+          <div className='ml-1 hidden md:block md:text-sm lg:text-base font-semibold dark:text-white'>Fellowship of Grace A/G</div>
+          <div className='ml-1 block md:hidden font-bold dark:text-white'>FOGA</div>
+        </div>
+      </Link>
+      <form>
+        <TextInput type='text' placeholder='Search...'
+        rightIcon={AiOutlineSearch} className='hidden lg:inline' />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
+        </Button>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue'>
+            Sign In
+          </Button> 
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse >
+     
+        <Link to='/'>
+        <Navbar.Link active={path === "/"} as={'div'}>
+          Home
+          </Navbar.Link>
+        </Link>
+        
+        
+        <Link to='/about'>
+        <Navbar.Link active={path === "/about"} as={'div'}>
+        About
+        </Navbar.Link>
+          
+        </Link>
+        
+        
+        <Link to='/contact-us' >
+        <Navbar.Link active={path === "/contact-us"} as={'div'}>
+        Contact Us
+        </Navbar.Link>
+        </Link>
+        
+      
+        <Link to='/project'>
+        <Navbar.Link active={path === "/project"} as={'div'}>
+        Project
+        </Navbar.Link>
+        </Link>
+     </Navbar.Collapse>
+    </Navbar>
+  );
+}
