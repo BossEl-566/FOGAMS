c14c8ff913cab9ccdb3dabe056e3bf40631a2d5a
Boss El <edatsomor010@st.ug.edu.gh>
2024-08-27 23:28:42 +0700

    chore: Add dotenv and update MongoDB dependencies
    This commit adds the `dotenv` package as a dependency and updates the `mongodb` and `mongoose` dependencies to their latest versions. It also configures the MongoDB connection in the `api/index.js` file using the `process.env.MONGO` environment variable.
    
    ```
    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000..');
-});
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to database successfully');
+  })
+  .catch((error) => {   
+    console.log('Error:', error);
+  });
+  const app = express();
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+
+//mongodb+srv://datsomorelliot56:qksRy88UwJ7KszZ4@cluster0.pem06.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
