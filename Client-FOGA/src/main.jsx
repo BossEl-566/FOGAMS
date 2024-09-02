6751616bacc2a3ce38502f713f2275c890cb128d
Boss El <edatsomor010@st.ug.edu.gh>
2024-09-02 11:00:44 +0700

    added redux persist

--- Client-FOGA/src/main.jsx
+++ Client-FOGA/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './radux/store.js'
-import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from './radux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
+  </PersistGate>
 );
